873ebd49170d210d764f18365ef52882848d5859
JkarthikJ <44233651+JkarthikJ@users.noreply.github.com>
2018-10-17 06:57:37 -0700

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char s;
+    scanf("%c",&s);
+    if((s>='a'&&s<='z')||(s>='A'&&s<='Z'))
+    {
+    if((s=='a'||s=='e'||s=='i'||s=='o'||s=='u')||(s=='A'||s=='E'||s=='I'||s=='O'||s=='U'))
+    printf("Vowel");
+    else 
+    printf("Consonant");
+    }
+    else
+    printf("Invalid");
+    return 0;
+}
