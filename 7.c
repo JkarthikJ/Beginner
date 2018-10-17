a34c832fb4745b5f94ffc5764afe80e85a48a4b7
JkarthikJ <44233651+JkarthikJ@users.noreply.github.com>
2018-10-17 07:23:23 -0700

    Update 7.c

--- 7.c
+++ 7.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
+    int n1,i;
+    scanf("%d",&n1);
+    for(i=0;i<n1;i++)
     printf("Hello\n");
     return 0;
 }
